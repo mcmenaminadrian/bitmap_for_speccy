c83dc161b34afe817ed47abacbf5bad132c73888
Egbert Eich <eich@suse.de>
2004-02-26 09:23:53 +0000

    Importing vendor version xf86-4_3_99_903 on Wed Feb 26 01:21:00 PST 2004

--- atobm.c
+++ atobm.c
@@ -26,7 +26,7 @@ other dealings in this Software without prior written authorization
 from The Open Group.
 
 */
-/* $XFree86: xc/programs/bitmap/atobm.c,v 3.5 2001/12/14 20:00:41 dawes Exp $ */
+/* $XFree86$ */
 
 /*
  * atobm - ascii to bitmap filter
