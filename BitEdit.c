fb4778c1a6a114df03f3cefc87e747fab983355f
Alan Coopersmith <alan.coopersmith@sun.com>
2009-09-22 19:30:35 -0700

    Strip RCS/CVS ids
    Signed-off-by: Alan Coopersmith <alan.coopersmith@sun.com>

--- BitEdit.c
+++ BitEdit.c
@@ -1,4 +1,3 @@
-/* $Xorg: BitEdit.c,v 1.4 2001/02/09 02:05:28 xorgcvs Exp $ */
 /*
 
 Copyright 1989, 1998  The Open Group
@@ -26,7 +25,6 @@ other dealings in this Software without prior written authorization
 from The Open Group.
 
 */
-/* $XFree86: xc/programs/bitmap/BitEdit.c,v 1.6tsi Exp $ */
 
 /*
  * Author:  Davor Matic, MIT X Consortium
--- Bitmap.c
+++ Bitmap.c
@@ -1,4 +1,3 @@
-/* $Xorg: Bitmap.c,v 1.4 2001/02/09 02:05:28 xorgcvs Exp $ */
 /*
 
 Copyright 1989, 1998  The Open Group
@@ -26,7 +25,6 @@ other dealings in this Software without prior written authorization
 from The Open Group.
 
 */
-/* $XFree86: Bitmap.c,v 1.5 2001/12/26 21:39:34 paulo Exp $ */
 
 /*
  * Author:  Davor Matic, MIT X Consortium
